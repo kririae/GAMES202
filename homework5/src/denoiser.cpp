ea613323577426b4ce47f8110ee030fbe9749797
kririae <kriaeth@outlook.com>
2021-10-31 12:28:42 +0800

    fin: hw5

--- homework5/src/denoiser.cpp
+++ homework5/src/denoiser.cpp
@@ -1,5 +1,9 @@
 #include "denoiser.h"
 
+#include <omp.h>
+
+#include <cassert>
+
 Denoiser::Denoiser() : m_useTemportal(false) {}
 
 void Denoiser::Reprojection(const FrameInfo &frameInfo) {
@@ -9,31 +13,71 @@ void Denoiser::Reprojection(const FrameInfo &frameInfo) {
       m_preFrameInfo.m_matrix[m_preFrameInfo.m_matrix.size() - 1];
   Matrix4x4 preWorldToCamera =
       m_preFrameInfo.m_matrix[m_preFrameInfo.m_matrix.size() - 2];
-#pragma omp parallel for
+
+#pragma omp parallel for default(none) shared(height, width, frameInfo, preWorldToScreen)
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
-      // TODO: Reproject
       m_valid(x, y) = false;
-      m_misc(x, y) = Float3(0.f);
+      m_misc(x, y) = Float3(0.0f);
+
+      const int obj_id = static_cast<int>(frameInfo.m_id(x, y));
+      if (obj_id < 0) continue;
+
+      const auto &M_p_1 = Inverse(frameInfo.m_matrix[obj_id]);
+      auto P = frameInfo.m_position(x, y);
+      P = M_p_1(P, Float3::EType::Point);
+
+      const auto &Ps = preWorldToScreen(P, Float3::EType::Point);
+      // const auto &Pc = preWorldToCamera(P, Float3::EType::Point);
+      if (0 <= Ps.x && Ps.x < width && 0 <= Ps.y && Ps.y < height &&
+          int(m_preFrameInfo.m_id(int(Ps.x), int(Ps.y))) == obj_id) {
+        m_valid(x, y) = true;
+        m_misc(x, y) = m_accColor(Ps.x, Ps.y);
+      }
     }
   }
+
   std::swap(m_misc, m_accColor);
 }
 
 void Denoiser::TemporalAccumulation(const Buffer2D<Float3> &curFilteredColor) {
   int height = m_accColor.m_height;
   int width = m_accColor.m_width;
-  int kernelRadius = 3;
-#pragma omp parallel for
+  constexpr int kernelRadius = 3;
+
+#pragma omp parallel for default(none) shared(height, width, curFilteredColor)
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
-      // TODO: Temporal clamp
+      int cnt = 0;
+      Float3 E, E2, Var;
       Float3 color = m_accColor(x, y);
-      // TODO: Exponential moving average
-      float alpha = 1.0f;
+
+      for (int i = -kernelRadius; i <= kernelRadius; ++i) {
+        for (int j = -kernelRadius; j <= kernelRadius; ++j) {
+          // Traverse within the kernel
+          const int dest_x = x + i, dest_y = y + j;
+          if (dest_x < 0 || dest_x >= width || dest_y < 0 || dest_y >= height) continue;
+
+          const auto &X = m_accColor(dest_x, dest_y);
+          E += X;
+          E2 += X * X;
+          ++cnt;
+        }
+      }
+
+      constexpr float k = 0.1f;
+      E /= float(cnt);
+      E2 /= float(cnt);
+      Var = E2 - E * E;
+
+      color = Clamp(color, E - Var * k, E + Var * k);
+
+      float alpha = 0.9;
+      if (!m_valid(x, y)) alpha = 1.0f;
       m_misc(x, y) = Lerp(color, curFilteredColor(x, y), alpha);
     }
   }
+
   std::swap(m_misc, m_accColor);
 }
 
@@ -41,19 +85,54 @@ Buffer2D<Float3> Denoiser::Filter(const FrameInfo &frameInfo) {
   int height = frameInfo.m_beauty.m_height;
   int width = frameInfo.m_beauty.m_width;
   Buffer2D<Float3> filteredImage = CreateBuffer2D<Float3>(width, height);
-  int kernelRadius = 16;
-#pragma omp parallel for
+
+  constexpr int kernelRadius = 16;
+#pragma omp parallel for default(none) shared(width, height, frameInfo, filteredImage)
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
-      // TODO: Joint bilateral filter
-      filteredImage(x, y) = frameInfo.m_beauty(x, y);
+      float kernelSum = 0.0f;
+      Float3 result;
+      const auto &C_i = frameInfo.m_beauty(x, y);
+      const auto &N_i = frameInfo.m_normal(x, y);
+      const auto &P_i = frameInfo.m_position(x, y);
+
+      for (int i = -kernelRadius; i <= kernelRadius; ++i) {
+        for (int j = -kernelRadius; j <= kernelRadius; ++j) {
+          // Traverse within the kernel
+          const int dest_x = x + i, dest_y = y + j;
+          if (dest_x < 0 || dest_x >= width || dest_y < 0 || dest_y >= height) continue;
+
+          const auto &C_j = frameInfo.m_beauty(dest_x, dest_y);
+          const auto &N_j = frameInfo.m_normal(dest_x, dest_y);
+          const auto &P_j = frameInfo.m_position(dest_x, dest_y);
+
+          float J = 0.0f;
+          float dist = SqrDistance(P_i, P_j);
+          if (i == 0 && j == 0) goto end;
+
+          // Calculate kernel
+          J += dist / (2 * m_sigmaCoord);
+          J += SqrDistance(C_i, C_j) / (2 * m_sigmaColor);
+          J += powf(SafeAcos(Dot(N_i, N_j)), 2) / (2 * m_sigmaNormal);
+          if (dist > 0.0f)
+            J += powf(Dot(N_i, (P_j - P_i) / sqrtf(dist)), 2) / (2 * m_sigmaPlane);
+
+        end:
+          J = expf(-J);
+          kernelSum += J;
+          result += frameInfo.m_beauty(dest_x, dest_y) * J;
+        }
+      }
+
+      assert(kernelSum != 0.0f);
+      filteredImage(x, y) = result / kernelSum;  // Energy preservation
     }
   }
+
   return filteredImage;
 }
 
-void Denoiser::Init(const FrameInfo &frameInfo,
-                    const Buffer2D<Float3> &filteredColor) {
+void Denoiser::Init(const FrameInfo &frameInfo, const Buffer2D<Float3> &filteredColor) {
   m_accColor.Copy(filteredColor);
   int height = m_accColor.m_height;
   int width = m_accColor.m_width;
@@ -61,9 +140,7 @@ void Denoiser::Init(const FrameInfo &frameInfo,
   m_valid = CreateBuffer2D<bool>(width, height);
 }
 
-void Denoiser::Maintain(const FrameInfo &frameInfo) {
-  m_preFrameInfo = frameInfo;
-}
+void Denoiser::Maintain(const FrameInfo &frameInfo) { m_preFrameInfo = frameInfo; }
 
 Buffer2D<Float3> Denoiser::ProcessFrame(const FrameInfo &frameInfo) {
   // Filter current frame
@@ -71,6 +148,7 @@ Buffer2D<Float3> Denoiser::ProcessFrame(const FrameInfo &frameInfo) {
   filteredColor = Filter(frameInfo);
 
   // Reproject previous frame color to current
+  // m_useTemportal = false;
   if (m_useTemportal) {
     Reprojection(frameInfo);
     TemporalAccumulation(filteredColor);
@@ -80,8 +158,7 @@ Buffer2D<Float3> Denoiser::ProcessFrame(const FrameInfo &frameInfo) {
 
   // Maintain
   Maintain(frameInfo);
-  if (!m_useTemportal) {
-    m_useTemportal = true;
-  }
+  if (!m_useTemportal) m_useTemportal = true;
+
   return m_accColor;
 }
--- homework5/src/main.cpp
+++ homework5/src/main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <fstream>
 #include <string>
 
@@ -12,22 +13,21 @@ std::vector<Matrix4x4> ReadMatrix(const std::string &filename) {
   int shapeNum;
   is.read(reinterpret_cast<char *>(&shapeNum), sizeof(int));
   std::vector<Matrix4x4> matrix(shapeNum + 2);
-  for (int i = 0; i < shapeNum + 2; i++) {
+  for (int i = 0; i < shapeNum + 2; i++)
     is.read(reinterpret_cast<char *>(&matrix[i]), sizeof(Matrix4x4));
-  }
   is.close();
   return matrix;
 }
 
 FrameInfo LoadFrameInfo(const filesystem::path &inputDir, const int &idx) {
-  Buffer2D<Float3> beauty = ReadFloat3Image(
-      (inputDir / ("beauty_" + std::to_string(idx) + ".exr")).str());
-  Buffer2D<Float3> normal = ReadFloat3Image(
-      (inputDir / ("normal_" + std::to_string(idx) + ".exr")).str());
-  Buffer2D<Float3> position = ReadFloat3Image(
-      (inputDir / ("position_" + std::to_string(idx) + ".exr")).str());
-  Buffer2D<float> depth = ReadFloatImage(
-      (inputDir / ("depth_" + std::to_string(idx) + ".exr")).str());
+  Buffer2D<Float3> beauty =
+      ReadFloat3Image((inputDir / ("beauty_" + std::to_string(idx) + ".exr")).str());
+  Buffer2D<Float3> normal =
+      ReadFloat3Image((inputDir / ("normal_" + std::to_string(idx) + ".exr")).str());
+  Buffer2D<Float3> position =
+      ReadFloat3Image((inputDir / ("position_" + std::to_string(idx) + ".exr")).str());
+  Buffer2D<float> depth =
+      ReadFloatImage((inputDir / ("depth_" + std::to_string(idx) + ".exr")).str());
   Buffer2D<float> id =
       ReadFloatImage((inputDir / ("ID_" + std::to_string(idx) + ".exr")).str());
   std::vector<Matrix4x4> matrix =
@@ -37,31 +37,43 @@ FrameInfo LoadFrameInfo(const filesystem::path &inputDir, const int &idx) {
   return frameInfo;
 }
 
-void Denoise(const filesystem::path &inputDir,
-             const filesystem::path &outputDir, const int &frameNum) {
+void Denoise(const filesystem::path &inputDir, const filesystem::path &outputDir,
+             const int &frameNum) {
   Denoiser denoiser;
   for (int i = 0; i < frameNum; i++) {
     std::cout << "Frame: " << i << std::endl;
     FrameInfo frameInfo = LoadFrameInfo(inputDir, i);
     Buffer2D<Float3> image = denoiser.ProcessFrame(frameInfo);
-    std::string filename =
-        (outputDir / ("result_" + std::to_string(i) + ".exr")).str();
+    std::string filename = (outputDir / ("result_" + std::to_string(i) + ".exr")).str();
     WriteFloat3Image(image, filename);
   }
 }
 
+std::string getPath(const std::string &target, int depth) {
+  std::string path = target;
+  for (int i = 0; i < depth; ++i) {
+    FILE *file = fopen(path.c_str(), "r");
+    if (file) {
+      fclose(file);
+      return path;
+    }
+
+    path = "../" + path;
+  }
+
+  LOG("failed to get file: " + target);
+  return target;
+}
 int main() {
   // Box
-  filesystem::path inputDir("examples/box/input");
-  filesystem::path outputDir("examples/box/output");
-  int frameNum = 20;
+  // filesystem::path inputDir(getPath("examples/box/input", 5));
+  // filesystem::path outputDir(getPath("examples/box/output", 5));
+  // int frameNum = 20;
 
-  /*
   // Pink room
-  filesystem::path inputDir("examples/pink-room/input");
-  filesystem::path outputDir("examples/pink-room/output");
+  filesystem::path inputDir(getPath("examples/pink-room/input", 5));
+  filesystem::path outputDir(getPath("examples/pink-room/output", 5));
   int frameNum = 80;
-  */
 
   Denoise(inputDir, outputDir, frameNum);
   return 0;
